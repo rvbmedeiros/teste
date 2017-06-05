87def4eed0ae2b8323fd27f67facc4c909da2900
Renato e Veronica <Renato e Veronica@RENATO-VERONICA>
2017-06-04 22:01:44 -0300

    novo main com int

--- teste/src/main/java/teste/teste/App.java
+++ teste/src/main/java/teste/teste/App.java
@@ -4,10 +4,13 @@ package teste.teste;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Helasdasloasdasdasd!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Helasdasloasdasdasd!");
+	}
+
+	public void main(Integer i) {
+		System.out.println("Helasdasloasdasdasd!");
+	}
 }
